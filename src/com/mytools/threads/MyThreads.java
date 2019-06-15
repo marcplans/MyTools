27b0d438313b94ec19dc7cbed3dc8ea2dc9835b8
marcplans <x@x-System-Product-Name>
2019-06-15 11:16:19 +0200

    190615 threads defined by lambdas and anonymous class

---
+++ src/com/mytools/threads/MyThreads.java
@@ -0,0 +1,37 @@
+package com.mytools.threads;
+
+public class MyThreads {
+
+	public static void main(String[] args) {
+
+		// Create a runnable object and override
+		// run method in anonymous class
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run()
+            {
+                //perform some work inside the thread
+                System.out.println("Hello from "+
+                        Thread.currentThread().getName()
+                        + " NOT USING LAMBDA");
+            }
+        };
+        
+        // Create a runnable object using lambda notation
+        Runnable r2 = () -> System.out.println("Hello from "
+                + Thread.currentThread().getName()+" USING LAMBDA "
+                + "notation");
+        
+        // Create and start a thread using the first runnable object
+        // This thread is also given a name in the arguments
+        Thread t1 = new Thread(r1, "Thread t1");
+        t1.start();
+        
+        // Create and start a second thread using the runnable 
+        // object with lambda notation and not given a name
+        Thread t2 = new Thread(r2);
+        t2.start();
+	}
+	
+	
+}
